3b857275ccc6e6ddad766f6c7be77182c74b8f74
Ilya <166957447+benzopilich@users.noreply.github.com>
2024-09-03 20:02:43 +0300

    BigDecimal and Double in one

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
 import java.math.BigDecimal;
 import java.math.BigInteger;
 import java.util.Scanner;
+import java.util.Formatter;
 
 public class Main {
 
@@ -15,12 +16,23 @@ public class Main {
         }
         System.out.println("Введите k (точность): ");
         int k = in.nextInt();
-        BigDecimal LongX=new BigDecimal(x);
-        Rad Calc = new Rad(LongX, k);
-        BigDecimal res = Calc.Arctan();
-        System.out.println("Мой atan = " + res + "\n");
+        Rad Calc = new Rad(x, k);
+        double res=Calc.Arctan_d();
+        System.out.println("Double atan = " + res + "\n");
+        BigDecimal Big_res = Calc.Arctan_Bd();
+        System.out.println("BigDecimal atan = " + Big_res + "\n");
         BigDecimal LongRes1= new BigDecimal(Math.atan(x));
         System.out.println("Встроенный atan = " + LongRes1 + "\n");
+
+        System.out.println("Форматы:");
+        Formatter formatter = new Formatter();
+        formatter.format("Восьмеричное представление k: %#o%n",k);
+        formatter.format("Шестнадцатеричное представление k: %#x%n",k);
+        StringBuilder sb=new StringBuilder("+010.");
+        sb.append(Integer.valueOf(k+1).toString());
+        formatter.format("Число с минимальной шириной поля %"+ sb +"f%n",res);
+        System.out.println(formatter);
+        formatter.close();
         in.close();
     }
 }
--- src/Rad.java
+++ src/Rad.java
@@ -3,28 +3,49 @@ import java.math.RoundingMode;
 
 public class Rad {
     private int k;
-    private BigDecimal x;
+    private double x;
 
-    public Rad(BigDecimal x1, int k1) {
+    public Rad(double x1, int k1) {
         this.k = k1;
         this.x = x1;
     }
 
-    public BigDecimal Arctan() {
+    public double Arctan_d(){
+        double eps = Math.pow(10,-k);
+        double sum = x;
+        int t=3;
+        double predSum=x;
+        for(int i=1; ;i++)
+        {
+            sum+=Math.pow(-1,i)*Math.pow(x,t)/t;
+            if(Math.abs(sum-predSum)<eps) {
+                BigDecimal res=new BigDecimal(sum);
+                res=res.setScale(k+1,RoundingMode.HALF_UP);
+                sum=res.doubleValue();
+                break;
+            }
+            t+=2;
+            predSum=sum;
+        }
+        return sum;
+    }
+
+    public BigDecimal Arctan_Bd() {
         double eps = Math.pow(10, -k);
         BigDecimal LongEps = new BigDecimal(eps);
-        BigDecimal sum = x;
+        BigDecimal sum = new BigDecimal(x);
         int t = 3;
-        BigDecimal predSum = x;
+        BigDecimal predSum = new BigDecimal(x);
 
         for (int i = 1; ; i++) {
-            BigDecimal tempX = x.pow(t);
+            BigDecimal tempX = BigDecimal.valueOf(Math.pow(x,t));
             tempX = tempX.multiply(BigDecimal.valueOf(Math.pow(-1, i)));
-            tempX = tempX.divide(BigDecimal.valueOf(t), k+1, RoundingMode.HALF_EVEN);
+            tempX = tempX.divide(BigDecimal.valueOf(t), k+1, RoundingMode.HALF_UP);
             sum = sum.add(tempX);
 
             BigDecimal diff = sum.subtract(predSum).abs();
-            if (diff.compareTo(LongEps) == -1) {
+            if (diff.compareTo(LongEps) < 0) {
+                sum=sum.setScale(k+1,RoundingMode.HALF_UP);
                 break;
             }
 
